3c0188b3241b77f58d730fe5a4b7c97af7904307
Adam <adam3692@hotmail.co.uk>
2015-11-13 17:52:49 +0000

    Updated database to take username | added buttons to main page | still needs fixes!

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@
 ActiveRecord::Schema.define(version: 20151112162335) do
 
   create_table "users", force: :cascade do |t|
+    t.string   "username",               default: "", null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
